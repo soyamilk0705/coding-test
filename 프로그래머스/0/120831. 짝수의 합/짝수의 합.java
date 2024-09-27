44d47aaa7d859ab8bebbfddb7d8fa97e2419f791
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-09-27 16:02:35 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(int n) { 
         int answer = 0;
         
-        for(int i= 0; i<=n; i++){
+        for(int i=2; i<=n; i+=2){
              if (i % 2 == 0){
                  answer += i;
              }
