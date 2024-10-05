c08d520471593d5836a0fcb885b227e699f49e72
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-10-05 11:38:47 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,19 +2,14 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String s) {
-        if (s.length() != 4 && s.length() != 6){
-            return false;
-        }
-        
-        char[] ch = s.toCharArray();
-        for(char c: ch){
-           if (c < '0' || c > '9'){
+        if (s.length() == 4 || s.length() == 6){
+            try{
+                int x = Integer.parseInt(s);
+                return true;
+            } catch(NumberFormatException e){
                 return false;
-           }
-            continue;
+            }
         }
-        
-        
-        return true;
+        return false;
     }
 }
