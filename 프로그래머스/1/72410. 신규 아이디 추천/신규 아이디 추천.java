86e80e21bb6dbcccc8ed05a24e287abcaada49e7
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-10-08 17:33:27 +0900

    [level 1] Title: 신규 아이디 추천, Time: 4.40 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        // 1단계
+        new_id = new_id.toLowerCase();
+        
+        // 2단계
+        String[] enableStr = {"~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", 
+                            "+", "[", "{", "]", "}", ":", "?", ",", "<", ">", "/"};
+        
+        for(int i=0; i<enableStr.length; i++){
+            if(new_id.contains(enableStr[i])){
+                new_id = new_id.replace(enableStr[i], "");
+            }    
+        }
+        
+        // 3단계
+        while (new_id.contains("..")){
+            new_id = new_id.replace("..", ".");
+        }
+        
+        // 4단계
+        if(new_id.startsWith(".")){
+            new_id = new_id.substring(1, new_id.length());
+        }
+        
+        if(new_id.endsWith(".")){
+            new_id = new_id.substring(0, new_id.length() - 1);
+        }
+        
+        // 5단계
+        if(new_id.equals("")){
+            new_id = "a";
+        }
+        
+        // 6단계
+        if(new_id.length() >=16){
+            new_id = new_id.substring(0, 15);
+            if (new_id.endsWith(".")){
+                new_id = new_id.substring(0, 14);
+            }
+        } else if(new_id.length() <= 2){ // 7단계
+            while(new_id.length() < 3){
+                new_id += new_id.substring(new_id.length()-1);
+            }
+        }
+        
+        System.out.println(new_id);
+        
+        return new_id;
+    }
+}
