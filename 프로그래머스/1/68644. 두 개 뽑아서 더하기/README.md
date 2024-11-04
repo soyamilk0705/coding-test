d78dbbe7f7d91aedf6f9056ecc1a9ca332a11731
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-11-04 10:59:37 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.76 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644?language=java) 
 
 ### 성능 요약
 
-메모리: 69.3 MB, 시간: 5.17 ms
+메모리: 80.5 MB, 시간: 1.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 10:50:57
+2024년 11월 04일 10:59:34
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,41 +1,30 @@
 import java.util.*;
 
 class Solution {
-    private static List<Integer> result = new ArrayList<>();
-    private static boolean[] visited;
+    private static Set<Integer> result = new HashSet<>();
     
     public int[] solution(int[] numbers) {
-        visited = new boolean[numbers.length];
-        add(numbers, 0);
+        int[] answer;
         
-        Collections.sort(result);
         
-        int[] answer = new int[result.size()];
-        
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i);
+        for(int i=0; i<numbers.length; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                result.add(numbers[i] + numbers[j]);
+            }
         }
         
+        answer = new int[result.size()];
+        int i=0;
+        
+        Iterator iter = result.iterator();
+        while(iter.hasNext()){
+            answer[i] = (int)iter.next();
+            i++;
+        }
+        
+        Arrays.sort(answer);
         
         return answer;
     }
     
-    public static void add(int[] numbers, int i){
-        visited[i] = true;
-        
-        for(int k=i+1; k<numbers.length; k++){
-            int sum = numbers[i] + numbers[k];
-            
-            if(!result.contains(sum)){
-                result.add(sum);
-            }
-            
-        }
-        
-        for(int j=0; j<visited.length; j++){
-            if(!visited[j]){
-                add(numbers, i+1);
-            }
-        }
-    }
 }
