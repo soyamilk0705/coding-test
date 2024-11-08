e1e8e9f7e00bcabac8fcc8d00c165d672259dddb
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-11-08 10:56:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int total = 45;
+        
+        for(int i=0; i<numbers.length; i++){
+            total -= numbers[i];
+        }
+        
+        return total;
+    }
+}
