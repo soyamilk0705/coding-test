528cf214dcbb13e2cf2d042ed90fe794113bf148
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-11-19 23:34:55 +0900

    [level 2] Title: 기능개발, Time: 3.75 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
 
 ### 성능 요약
 
-메모리: 77.2 MB, 시간: 11.18 ms
+메모리: 78.9 MB, 시간: 3.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 19일 23:30:24
+2024년 11월 19일 23:34:52
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,37 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> answer = new ArrayList<>();
-        Queue<Integer> queue = new LinkedList<>();
+        int[] dayOfend = new int[100];
+        int day = -1;
         
-        for(int i=0; i<speeds.length; i++){
-            int remainPro = 100 - progresses[i];
-            
-            if(remainPro % speeds[i] != 0){
-                queue.offer(remainPro / speeds[i] + 1);
-            } else{
-                queue.offer(remainPro / speeds[i]);
+        for(int i=0; i<progresses.length; i++){
+            while(progresses[i] + (day*speeds[i]) < 100){
+                day++;
             }
+            dayOfend[day]++;
         }
-        
-        int day = queue.poll();
-        int value = 1;
-        
-        while(!queue.isEmpty()){
-            int nextDay = queue.poll();
-            
-            if(nextDay > day){
-                answer.add(value);
-                day = nextDay;
-                value = 1;
-            } else{
-                value++;
-                continue;
-            }
-        }
-        
-        answer.add(value);
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
