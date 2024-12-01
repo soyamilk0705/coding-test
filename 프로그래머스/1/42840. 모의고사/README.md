15a321018765562234fa33fd0951a526d270cc8f
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2024-12-01 18:15:52 +0900

    [level 1] Title: 모의고사, Time: 17.85 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 3.53 ms
+메모리: 71.6 MB, 시간: 17.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 18:34:13
+2024년 12월 01일 18:15:50
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,27 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] stu1 = {1, 2, 3, 4, 5};
-        int[] stu2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] stu3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] score = new int[3];
-        
+        int[] grades = new int[3];
+    
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == stu1[i%5]) score[0]++;
-            if(answers[i] == stu2[i%8]) score[1]++;
-            if(answers[i] == stu3[i%10]) score[2]++;
+            if(answers[i] == person1[i%5]){
+                grades[0]++;
+            }
+            if(answers[i] == person2[i%8]){
+                grades[1]++;
+            }
+            if(answers[i] == person3[i%10]){
+                grades[2]++;
+            }
         }
         
-        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        int max = Arrays.stream(grades).max().orElse(0);
+        List<Integer> answer = new ArrayList<>();
         
-        List<Integer> list = new ArrayList<>();
-        for(int i=0; i<3; i++){
-            if(max == score[i]) list.add(i+1);
+        for(int i=0; i<grades.length; i++){
+            if(grades[i] == max){
+                answer.add(i+1);
+            }
         }
         
-        return list.stream().mapToInt(i->i.intValue()).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+        
     }
-    
-    
 }
