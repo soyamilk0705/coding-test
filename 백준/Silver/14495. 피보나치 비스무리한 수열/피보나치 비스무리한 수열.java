778a777cb4db3468eb80889879a1aac3b2a41649
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2025-04-01 16:58:46 +0900

    [Silver IV] Title: 피보나치 비스무리한 수열, Time: 104 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/14495. 피보나치 비스무리한 수열/피보나치 비스무리한 수열.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		long[] dp = new long[117];
+		
+		dp[1] = dp[2] = dp[3] = 1;
+		
+		for(int i=4; i<=n; i++) {
+			dp[i] = dp[i-1] + dp[i-3];
+		}
+		
+		System.out.println(dp[n]);
+		
+	}
+	
+	
+}
