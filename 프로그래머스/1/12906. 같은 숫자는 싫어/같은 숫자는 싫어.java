7b61e1fc66e5c808f73806126b6ca90600e25c35
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-02-04 18:35:24 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 72.99 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -4,23 +4,27 @@ public class Solution {
     public int[] solution(int []arr) {
         Stack<Integer> stack = new Stack<>();
         
-        for(int i=0; i<arr.length; i++){
-            if(stack.isEmpty()){
-                stack.push(arr[i]);
-            } else{
-                int temp = stack.peek();
-                if(temp != arr[i]){
+        stack.push(arr[0]);
+        
+        for(int i=1; i<arr.length; i++){
+            if(!stack.isEmpty()){
+                int tmp = stack.peek();
+                
+                if(tmp != arr[i]){
                     stack.push(arr[i]);
-                } 
+                }
+            } else{
+                stack.push(arr[i]);
             }
         }
         
         int[] answer = new int[stack.size()];
-        
         for(int i=stack.size()-1; i>=0; i--){
             answer[i] = stack.pop();
         }
         
+        
         return answer;
     }
+    
 }
