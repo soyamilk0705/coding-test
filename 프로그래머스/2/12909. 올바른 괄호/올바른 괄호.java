feadda56d551cff9bfa0e30835ceea56917aca22
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-02-05 17:46:41 +0900

    [level 2] Title: 올바른 괄호, Time: 20.45 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -5,17 +5,16 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         
         for(char c : s.toCharArray()){
-            if(c == ')' && !stack.isEmpty()){
-                stack.pop();
+            if(c == '('){
+                stack.push(c);
             } else{
-                stack.push('(');
+                if(stack.isEmpty()){
+                    return false;  
+                }
+                stack.pop(); 
             }
         }
         
-        if(stack.size() != 0){
-            return false;
-        }
-
-        return true;
+        return stack.isEmpty();
     }
 }
