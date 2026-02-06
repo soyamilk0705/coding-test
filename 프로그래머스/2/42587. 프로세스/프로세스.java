70a286fb09c1437c444a770d53470ec833487d16
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-02-06 17:14:52 +0900

    [level 2] Title: 프로세스, Time: 10.34 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,45 +1,48 @@
 import java.util.*;
 
-class Process{
-    int idx;
-    int value;
-    
-    public Process(int idx, int value){
-        this.idx = idx;
-        this.value = value;
-    }
-}
-
 class Solution {
+    static class Process {
+        int order;
+        int priority;
+        
+        public Process(int order, int priority){
+            this.order = order;
+            this.priority = priority;
+        }
+    }
+    
     public int solution(int[] priorities, int location) {
         int answer = 0;
         Queue<Process> queue = new LinkedList<>();
+        List<Integer> sortPri = new ArrayList<>();
+        
+        int priIdx = 0;
+        
         
         for(int i=0; i<priorities.length; i++){
-            queue.offer(new Process(i, priorities[i]));
+            Process p = new Process(i, priorities[i]);
+            queue.add(p);
+            sortPri.add(priorities[i]);
         }
         
+        Collections.sort(sortPri, Comparator.reverseOrder());
+        
         while(!queue.isEmpty()){
-            int max = 0;
-        
-            for(int i=0; i<queue.size(); i++){
-                Process tmp = queue.poll();
-                max = Math.max(max, tmp.value);
-                queue.offer(tmp);
-            }
-            
             Process p = queue.poll();
-        
-            if(p.value == max){
-                answer++;
-                if(p.idx == location){
-                    break;
+            
+            if(p.priority == sortPri.get(priIdx)) {
+                if(p.order == location){
+                    return answer+1;
+                } else {
+                    answer++;
+                    priIdx++;                
                 }
             } else{
                 queue.offer(p);
             }
-            
         }
+        
+        
         
         return answer;
     }
