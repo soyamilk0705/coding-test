f4abccb374547223c22a6889a41181abb0d32bff
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-05 01:33:58 +0900

    Time: 1 ms (99.9%), Space: 46.4 MB (57.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                char c = board[i][j];
+                
+                if (c == '.') continue;
+                
+                int num = c - '1';
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                
+                if(row[i][num] || col[j][num] || box[boxIndex][num]){
+                    return false;
+                }
+                
+                row[i][num] = true;
+                col[j][num] = true;
+                box[boxIndex][num] = true;
+            }
+        }
+        
+        return true;
+    }
+}
