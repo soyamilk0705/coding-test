1455153bf9f7768acae93b74c0f80ede179dcef5
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-17 17:41:28 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.60 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
 class Solution {
-    public class Robot{
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    
+    static class Robot {
         int x;
         int y;
         int cnt;
@@ -13,43 +16,36 @@ class Solution {
         }
     }
     
-    static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    
-    
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        visited = new boolean[n][m];
-        visited[0][0] = true;
-      
+        boolean[][] visited = new boolean[n][m];
+        
         if(n == 1 && m == 1){
             return 1;
         }
         
-        Robot robot = new Robot(0, 0, 1); 
         Queue<Robot> queue = new LinkedList<>();
-        queue.offer(robot);
+        queue.offer(new Robot(0, 0, 1));
         
         while(!queue.isEmpty()){
-            Robot r = queue.poll();
+            Robot robot = queue.poll();
             
             for(int i=0; i<4; i++){
-                int tx = dx[i] + r.x;
-                int ty = dy[i] + r.y;
+                int lx = robot.x + dx[i];
+                int ly = robot.y + dy[i];
                 
-                if(tx > -1 && tx < n && ty > -1 && ty < m && !visited[tx][ty] && maps[tx][ty] == 1){
-                    if(tx == n - 1 && ty == m - 1){
-                        return r.cnt+1;
+                if(lx >= 0 && ly >= 0 && lx < n && ly < m && !visited[lx][ly] && maps[lx][ly] == 1){
+                    
+                    if(lx == n-1 && ly == m-1){
+                        return robot.cnt+1; 
                     } else{
-                        visited[tx][ty] = true;
-                        queue.offer(new Robot(tx, ty, r.cnt+1));
+                        visited[lx][ly] = true;
+                        queue.offer(new Robot(lx, ly, robot.cnt+1));
                     }
                 }
             }
         }
-        
         
         return -1;
     }
