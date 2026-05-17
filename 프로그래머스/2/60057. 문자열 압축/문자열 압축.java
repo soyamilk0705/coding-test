f5294cbf3e2c03f706a2c411372611d609e52dff
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-17 16:05:11 +0900

    [level 2] Title: 문자열 압축, Time: 2.12 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -2,35 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
+        if(s.length() == 1){
+            return 1;
+        }
         int answer = s.length();
-      
+        
         for(int step=1; step<=s.length()/2; step++){
             StringBuilder sb = new StringBuilder();
-            String prefix = s.substring(0, step);
             int cnt = 1;
+            String prefix = s.substring(0, step);
             
             for(int j=step; j<s.length(); j+=step){
                 int len = Math.min(j+step, s.length());
-                String cur = s.substring(j, len);
+                String str = s.substring(j, len);
                 
-                if(cur.equals(prefix)){
-                    cnt++;   
+                if(str.equals(prefix)){
+                    cnt++;
                 } else{
                     if(cnt > 1){
                         sb.append(cnt);
                     }
-                    
                     sb.append(prefix);
                     
-                    prefix = cur;
                     cnt = 1;
+                    prefix = str;
                 }
             }
             
             if(cnt > 1){
                 sb.append(cnt);
             }
-        
             sb.append(prefix);
             answer = Math.min(sb.length(), answer);
         }
