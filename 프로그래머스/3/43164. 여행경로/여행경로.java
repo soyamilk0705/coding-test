982d3bd3c665cfd3a03feec01feadcc6f51b61c6
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-18 14:45:36 +0900

    [level 3] Title: 여행경로, Time: 0.30 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     static boolean[] visited;
-    static List<String> list = new ArrayList<>();
+    static List<String> airs = new ArrayList<>();
     static boolean finish = false;
     
     public void dfs(int cnt, String start, String[][] tickets){
@@ -14,7 +14,7 @@ class Solution {
         for(int i=0; i<tickets.length; i++){
             if(!visited[i] && tickets[i][0].equals(start)){
                 visited[i] = true;
-                list.add(tickets[i][1]);
+                airs.add(tickets[i][1]);
                 
                 dfs(cnt+1, tickets[i][1], tickets);
                 
@@ -23,25 +23,24 @@ class Solution {
                 }
                 
                 visited[i] = false;
-                list.remove(list.size() - 1);
+                airs.remove(airs.size() - 1);
             }
         }
-        
     }
     
     public String[] solution(String[][] tickets) {
         visited = new boolean[tickets.length];
-        Arrays.sort(tickets, (o1, o2) -> {
-            if(o1[0].equals(o2[0])) {
-                return o1[1].compareTo(o2[1]);
-            } 
-           
-            return o1[0].compareTo(o2[0]);
-       });
         
-        list.add("ICN");
+        Arrays.sort(tickets, (o1, o2) -> {
+            if(o1[0].equals(o2[0])){
+                return o1[1].compareTo(o2[1]);
+            }
+            return o1[0].compareTo(o2[0]);
+        });
+        
+        airs.add("ICN");
         dfs(0, "ICN", tickets);
         
-        return list.toArray(new String[list.size()]);
+        return airs.toArray(new String[airs.size()]);
     }
 }
