c4e3964d17f7cc1634290b2a516f4ff8b670be84
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-20 17:18:33 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,30 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 0;
-        Map<String, List<String>> map = new HashMap<>();
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
         
         for(String[] cloth : clothes){
-            if(map.containsKey(cloth[1])){
-                List<String> values = map.get(cloth[1]);
-                values.add(cloth[0]);
-                map.put(cloth[1], values);
-            } else{
-                map.put(cloth[1], new ArrayList<>(List.of(cloth[0])));
-            }
-            answer++;
+            map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
-        if(map.size() == 1){
-            return answer;
-        }
-        
-        int tmp = 1;
         for(String key : map.keySet()){
-            List<String> list = map.get(key);
-            tmp *= (list.size() + 1);
+            answer *= (map.get(key) + 1);
         }
         
-        return tmp - 1;
+        return answer - 1;
     }
 }
