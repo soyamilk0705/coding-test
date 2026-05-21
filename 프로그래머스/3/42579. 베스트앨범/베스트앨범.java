73fe846d2727d11cda5faa10331e1855469c0124
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-21 15:55:43 +0900

    [level 3] Title: 베스트앨범, Time: 3.60 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,58 +1,57 @@
 import java.util.*;
 
 class Solution {
+    class Record {
+        int idx;
+        int plays;
+        
+        public Record(int idx, int plays){
+            this.idx = idx;
+            this.plays = plays;
+        }
+        
+    }
+    
     public int[] solution(String[] genres, int[] plays) {
         Map<String, Integer> map = new HashMap<>();
-        ArrayList<Integer> answer = new ArrayList<>();
-        
+        Map<String, List<Record>> recordMap = new HashMap<>();
+        List<Integer> answer = new ArrayList<>();
         
         for(int i=0; i<genres.length; i++){
             map.put(genres[i], map.getOrDefault(genres[i], 0) + plays[i]);
-        }
-        
-        ArrayList<String> mapToGenres = new ArrayList<>();
-        
-        for(String item : map.keySet()){
-            mapToGenres.add(item);
-        }
-        mapToGenres.sort(((o1, o2) -> map.get(o2) - map.get(o1)));
-        
-        for(String item : mapToGenres){
-            ArrayList<music> musics = new ArrayList<>();
-            for(int i=0; i<genres.length; i++){
-                if(item.equals(genres[i])){
-                    musics.add(new music(i, plays[i]));
-                }
-            }
             
-            musics.sort(new Comparator<music>(){
-                @Override
-                public int compare(music o1, music o2){
-                    if(o1.play == o2.play){
-                        return o1.idx - o2.idx;
-                    }
-                    return o2.play - o1.play;
+            List<Record> records;
+            if(recordMap.containsKey(genres[i])){
+                records = recordMap.get(genres[i]);
+            } else{
+                records = new ArrayList<>();
+            }
+            records.add(new Record(i, plays[i]));
+            recordMap.put(genres[i], records);
+        }
+        
+        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
+        Collections.sort(list, (o1, o2) -> o2.getValue() - o1.getValue());
+        
+        for(Map.Entry<String, Integer> entry : list){
+            String key = entry.getKey();
+            List<Record> records = recordMap.get(key);
+            
+            Collections.sort(records, (o1, o2) -> {
+                if(o2.plays == o1.plays){
+                    return o1.idx - o2.idx;
                 }
+                return o2.plays - o1.plays;
             });
             
-            answer.add(musics.get(0).idx);
-            
-            if(musics.size() != 1){
-                answer.add(musics.get(1).idx);
+            for(int i=0; i<Math.min(2, records.size()); i++){
+                answer.add(records.get(i).idx);
             }
+            
         }
         
-        return answer.stream().mapToInt(i -> i).toArray();
-    }
-    
-    public static class music{
-        int idx;
-        int play;
-        
-        public music(int idx, int play){
-            this.idx = idx;
-            this.play = play;
-        }
-        
+        return answer.stream()
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
