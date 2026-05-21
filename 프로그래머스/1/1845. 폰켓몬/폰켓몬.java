6af8e8b42618f2fe73dad21249c2a75ad20df715
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-21 15:06:50 +0900

    [level 1] Title: 폰켓몬, Time: 2.74 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        int answer = 0;
         Set<Integer> set = new HashSet<>();
         
         for(int num : nums){
             set.add(num);
         }
         
-        if(set.size() <= nums.length / 2){
+        if(set.size() < nums.length / 2){
             return set.size();
         }
         
