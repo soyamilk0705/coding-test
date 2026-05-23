ee4d857fde20c10455fc45b0e2b25c1bf295f4f2
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-23 15:16:24 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
         int[] students = new int[n+1];
         Arrays.fill(students, 1);
         
@@ -12,27 +13,28 @@ class Solution {
         for(int r : reserve){
             students[r]++;
         }
-        
+       
         for(int i=1; i<=n; i++){
             if(students[i] == 0){
                 if(i > 1 && students[i-1] == 2){
+                    students[i]++;
                     students[i-1]--;
-                    students[i]++;
                 } else if(i < n && students[i+1] == 2){
-                    students[i+1]--;
                     students[i]++;
+                    students[i+1]--;
                 }
             }
+
         }
         
-        int answer = 0;
-        
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<students.length; i++){
             if(students[i] >= 1){
                 answer++;
             }
         }
         
+        
+    
         return answer;
     }
 }
