e7253f528a7ace64b5914d61d90fc281be33854f
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-24 16:16:07 +0900

    [level 2] Title: 가장 큰 수, Time: 1452.46 ms, Memory: 373 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        List<String> numList = new ArrayList<>();
+        String answer = "";
+        List<String> numberList = new ArrayList<>();
         
         for(int num : numbers){
-            numList.add(String.valueOf(num));
+            numberList.add(String.valueOf(num));
         }
         
-        numList.sort((a, b) -> (b+a).compareTo(a+b));
+        numberList.sort((a, b) -> (b+a).compareTo(a+b));
         
-        StringBuilder sb = new StringBuilder();
-        
-        for(String num : numList){
-            sb.append(num);
+        for(String num : numberList){
+            answer += num;
         }
         
-        if(sb.toString().startsWith("0")){
+        if(answer.startsWith("0")){
             return "0";
         }
         
-        return sb.toString();
+        
+        return answer;
     }
 }
