3e68fb0297c67e50119a96b3ee567f933b8bb2f8
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-24 16:38:11 +0900

    [level 3] Title: 베스트앨범, Time: 3.26 ms, Memory: 61.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,38 +1,39 @@
 import java.util.*;
 
 class Solution {
+    class Music {
+        int idx;
+        int cnt;
+        
+        public Music(int idx, int cnt){
+            this.idx = idx;
+            this.cnt = cnt;
+        }
+    }
     
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
         Map<String, Integer> totalMap = new HashMap<>();
-        Map<String, List<Integer>> musicMap = new HashMap<>();
+        Map<String, List<Music>> musicMap = new HashMap<>();
         
         for(int i=0; i<genres.length; i++){
             totalMap.put(genres[i], totalMap.getOrDefault(genres[i], 0) + plays[i]);
-            
-            musicMap.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(i);
+            musicMap.computeIfAbsent(genres[i], k -> new ArrayList<>())
+                .add(new Music(i, plays[i]));
         }
         
-        List<String> genreList = new ArrayList<>(totalMap.keySet());
-        genreList.sort((a, b) -> totalMap.get(b) - totalMap.get(a));
-    
+        List<String> genresSort = new ArrayList<>(totalMap.keySet());
+        genresSort.sort((a, b) -> totalMap.get(b) - totalMap.get(a));
         
-        for(String genre : genreList){
-            List<Integer> musics = musicMap.get(genre);
-            
-            musics.sort((a, b) -> {
-               if(plays[a] == plays[b]){
-                   return a - b;
-               }
-                return plays[b] - plays[a];
-            });
+        for(String genre : genresSort){
+            List<Music> musics = musicMap.get(genre);
+            musics.sort((a, b) -> b.cnt - a.cnt);
             
             for(int i=0; i<Math.min(2, musics.size()); i++){
-                answer.add(musics.get(i));
+                
+                answer.add(musics.get(i).idx);
             }
         }
-        
-        
         
         return answer.stream()
             .mapToInt(Integer::intValue)
