4922aed3c4ad74ff9eb615b216b5becc24ca7af9
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-24 17:58:22 +0900

    [level 2] Title: 구명보트, Time: 6.91 ms, Memory: 58.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,15 +4,14 @@ class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
         
-        int left = 0;
-        int right = people.length - 1;
         int answer = 0;
+        int left = 0;
+        int right = people.length-1;
         
         while(left <= right){
             if(people[left] + people[right] <= limit){
                 left++;
             }
-            
             right--;
             answer++;
         }
