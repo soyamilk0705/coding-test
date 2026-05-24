3047160af9a711094c31ee1cd11c1c02f77487fb
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-24 16:47:55 +0900

    [level 2] Title: 전화번호 목록, Time: 194.23 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,23 +3,19 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Set<String> set = new HashSet<>();
+        Set<String> phones = new HashSet<>(List.of(phone_book));
         
-        for(String phone : phone_book){
-            set.add(phone);
-        }
-        
-        
-        for(String phone : phone_book){
+        for(String phone : phones){
             for(int i=1; i<phone.length(); i++){
                 String prefix = phone.substring(0, i);
-
-                if(set.contains(prefix)){
+                
+                if(phones.contains(prefix)){
                     return false;
                 }
-                
             }
         }
+        
+        
         
         return answer;
     }
