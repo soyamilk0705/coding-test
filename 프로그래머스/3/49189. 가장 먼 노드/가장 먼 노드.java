500a278fdee6857146d473e3cccc71633a61b780
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-25 18:43:19 +0900

    [level 3] Title: 가장 먼 노드, Time: 17.13 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, -1);
+        
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] e : edge){
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(1);
+        dist[1] = 0;
+        
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+            
+            for(int next : graph.get(now)){
+                if(dist[next] == -1){
+                    dist[next] = dist[now] + 1;
+                    queue.offer(next);
+                }
+            }
+        }
+        
+        int max = 0;
+        
+        for(int d : dist){
+            max = Math.max(max, d);
+        }
+        
+        int answer = 0;
+        
+        for(int d : dist){
+            if(d == max){
+                answer++;
+            }
+        }
+        
+        
+    
+        return answer;
+    }
+}
