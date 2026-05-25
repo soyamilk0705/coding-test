da63674e0760ca1d491dcbe0aca11bd5ec53fc11
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-25 16:16:24 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,44 +5,20 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for(int i=0; i<commands.length; i++){
-            int j = commands[i][0] - 1;
-            int k = commands[i][1];
-            int[] arr = Arrays.copyOfRange(array, j, k);
-            quickSort(arr, 0, arr.length-1);
-            answer[i] = arr[commands[i][2] - 1];
+            int s = commands[i][0] - 1;
+            int e = commands[i][1] - 1;
+            int k = commands[i][2] - 1;
             
+            List<Integer> tmp = new ArrayList<>();
+            for(int j=s; j<=e; j++){
+                tmp.add(array[j]);
+            }
+            
+            Collections.sort(tmp);
+            answer[i] = tmp.get(k);
         }
+        
         
         return answer;
-    }
-    
-    public static void quickSort(int[] arr, int start, int end){
-        if(start < end){
-            int pivotIndex = partition(arr, start, end);
-            
-            quickSort(arr, start, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, end);
-        }
-    }
-    
-    public static int partition(int[] arr, int start, int end){
-        int pivot = arr[end];
-        int i = start - 1;
-        
-        for(int j=start; j<end; j++){
-            if(arr[j] < pivot){
-                i++;
-                swap(arr, i, j);
-            }
-        }
-       
-        swap(arr, i+1, end);
-        return i+1;
-    }
-    
-     private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
