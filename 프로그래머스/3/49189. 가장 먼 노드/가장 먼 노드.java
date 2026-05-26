1deca968457d45165d90c772d723b964b263c401
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-26 15:26:08 +0900

    [level 3] Title: 가장 먼 노드, Time: 19.96 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        List<List<Integer>> graph = new ArrayList<>();
+        int answer = 0;
         int[] dist = new int[n+1];
+        List<List<Integer>> graph = new ArrayList<>();
+        
         Arrays.fill(dist, -1);
         
         for(int i=0; i<=n; i++){
@@ -14,29 +16,28 @@ class Solution {
             graph.get(e[0]).add(e[1]);
             graph.get(e[1]).add(e[0]);
         }
-
+        
+        
         Queue<Integer> queue = new LinkedList<>();
         queue.offer(1);
         dist[1] = 0;
         
+        
         while(!queue.isEmpty()){
             int now = queue.poll();
             
-            for(int next : graph.get(now)){
-                if(dist[next] == -1){
-                    dist[next] = dist[now] + 1;
-                    queue.offer(next);
+            for(int g : graph.get(now)){
+                if(dist[g] == -1){
+                    dist[g] = dist[now] + 1;
+                    queue.offer(g);
                 }
             }
         }
         
         int max = 0;
-        
         for(int d : dist){
-            max = Math.max(max, d);
+            max = Math.max(d, max);
         }
-        
-        int answer = 0;
         
         for(int d : dist){
             if(d == max){
@@ -44,8 +45,6 @@ class Solution {
             }
         }
         
-        
-    
         return answer;
     }
 }
