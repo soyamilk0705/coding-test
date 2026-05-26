766ecda8551fdfeda18992c92422e90b35554b8c
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-26 17:02:57 +0900

    [level 2] Title: 조이스틱, Time: 0.15 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -3,24 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(String name) {
         int answer = 0;
-        int len = name.length();
-        int move = name.length() - 1;
-    
-        for(int i=0; i<len; i++){
-            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
-
-            // 연속된 A가 끝나는 지점 찾기
-            int next = i + 1;
-            while(next < len && name.charAt(next) == 'A'){
+        int length = name.length();
+        
+        int move = length - 1;
+        
+        for(int i=0; i<length; i++){
+            answer += Math.min(
+            name.charAt(i) - 'A',
+            'Z' - name.charAt(i) + 1);
+            
+            int next = i+1;
+            
+            while(next < length && name.charAt(next) == 'A'){
                 next++;
             }
-
-            move = Math.min(move, (i * 2) + len - next);
-            move = Math.min(move, (len - next) * 2 + i);
+            
+            move = Math.min(move, i * 2 + length - next);
+            move = Math.min(move, (length - next) * 2 + i);
         }
         
-        answer += move;
-
-        return answer;
+        return answer + move;
     }
 }
