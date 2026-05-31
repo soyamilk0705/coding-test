9c4b8354a3e3dba3e3ab4497ec29d2f6a5e11896
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-05-31 16:02:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.38 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,52 +1,51 @@
 import java.util.*;
 
-class Solution {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+class Point {
+    int x;
+    int y;
     
-    static class Robot {
-        int x;
-        int y;
-        int cnt;
-        
-        public Robot(int x, int y, int cnt){
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
     }
-    
+}
+
+class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int[][] dist = new int[n][m];
         
-        if(n == 1 && m == 1){
-            return 1;
-        }
-        
-        Queue<Robot> queue = new LinkedList<>();
-        queue.offer(new Robot(0, 0, 1));
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(0, 0));
+        dist[0][0] = 1;
         
         while(!queue.isEmpty()){
-            Robot robot = queue.poll();
+            Point now = queue.poll();
+            
+            if(now.x == n - 1 && now.y == m - 1){
+                break;
+            }
             
             for(int i=0; i<4; i++){
-                int lx = robot.x + dx[i];
-                int ly = robot.y + dy[i];
+                int nx = now.x + dx[i];
+                int ny = now.y + dy[i];
                 
-                if(lx >= 0 && ly >= 0 && lx < n && ly < m && !visited[lx][ly] && maps[lx][ly] == 1){
-                    
-                    if(lx == n-1 && ly == m-1){
-                        return robot.cnt+1; 
-                    } else{
-                        visited[lx][ly] = true;
-                        queue.offer(new Robot(lx, ly, robot.cnt+1));
-                    }
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1){
+                    maps[nx][ny] = 0;
+                    dist[nx][ny] = dist[now.x][now.y] + 1;
+                    queue.offer(new Point(nx, ny));
                 }
             }
         }
         
-        return -1;
+        if(dist[n - 1][m - 1] == 0){
+            return -1;
+        } else{
+            return dist[n - 1][m - 1];
+        }
+        
     }
 }
