03703fd097176b50893e69eef603274c9c217e6d
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-06-10 14:14:22 +0900

    [level 1] Title: 최소직사각형, Time: 0.60 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,22 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] sizes) {
-        int answer = 0;
         int weight = 0;
         int height = 0;
         
-        
-        for(int i=0; i<sizes.length; i++){
+        for(int[] card : sizes){
+            int w = Math.max(card[0], card[1]);
+            int h = Math.min(card[0], card[1]);
             
-            if(sizes[i][0] < sizes[i][1]){
-                weight = Math.max(weight, sizes[i][1]);
-                height = Math.max(height, sizes[i][0]);
-            } else {
-                weight = Math.max(weight, sizes[i][0]);
-                height = Math.max(height, sizes[i][1]);
-            }
-            
+            weight = Math.max(weight, w);
+            height = Math.max(height, h);
         }
-        
         
         return weight * height;
     }
