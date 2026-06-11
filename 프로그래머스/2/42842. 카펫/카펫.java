5ebbf22ddd7f9d24cea9cd61bcb6bb1c6b31ef36
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-06-11 15:11:02 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,17 +2,18 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
         
-        for(int height=1; height*height<=total; height++){
-            if(total % height != 0){
+        for(int i=1; i<=total; i++){
+            if(total % i != 0){
                 continue;
             }
             
-            int width = total / height;
+            int height = total / i;
             
-            if((width - 2) * (height - 2) == yellow){
-                return new int[]{width, height};
+            if((height-2) * (i-2) == yellow){
+                return new int[]{height, i};
             }
         }
+        
         
         return new int[]{0, 0};
     }
