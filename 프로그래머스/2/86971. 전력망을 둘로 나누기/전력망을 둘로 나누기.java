9683a81390e3cc59fbf9a6223a99fd7e306a4406
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-06-20 14:56:19 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.78 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,50 +1,48 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
         
-        for(int cut=0; cut<wires.length; cut++){
-            List<List<Integer>> graph = new ArrayList<>();
-            
-            for(int i=0; i<=n; i++){
-                graph.add(new ArrayList<>());
-            }
-            
-            for(int i=0; i<wires.length; i++){
-                if(i == cut) continue;
-                
-                int a = wires[i][0];
-                int b = wires[i][1];
-                
-                graph.get(a).add(b);
-                graph.get(b).add(a);
-            }
-            
-            // BFS
-            boolean[] visited = new boolean[n+1];
+        for(int i=0; i<wires.length; i++){
+            List<List<Integer>> tops = new ArrayList<>();
             Queue<Integer> queue = new LinkedList<>();
+            boolean[] visited = new boolean[n+1];
+            
+            for(int j=0; j<=n; j++){
+                tops.add(new ArrayList<>());
+            }
+            
+            for(int j=0; j<wires.length; j++){
+                if(i==j) continue;
+                
+                int a = wires[j][0];
+                int b = wires[j][1];
+                
+                tops.get(a).add(b);
+                tops.get(b).add(a);
+            }
             
             queue.offer(1);
             visited[1] = true;
-            
-            int count = 1;
-            
+            int cnt = 1;
+                
             while(!queue.isEmpty()){
                 int now = queue.poll();
                 
-                for(int next : graph.get(now)){
+                for(int next : tops.get(now)){
                     if(!visited[next]){
-                        visited[next] = true;
                         queue.offer(next);
-                        count++;
+                        cnt++;
+                        visited[next] = true;
                     }
                 }
             }
             
-            answer = Math.min(answer, Math.abs(count - (n - count)));
+            answer = Math.min(answer, Math.abs(cnt - (n - cnt)));
+            
         }
+        
         
         
         return answer;
