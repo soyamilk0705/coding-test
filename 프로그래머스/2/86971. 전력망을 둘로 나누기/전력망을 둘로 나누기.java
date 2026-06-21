25ce0b020450bd9e033b0009894089bdbe2bcfb6
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-06-21 18:21:35 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.12 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -5,46 +5,42 @@ class Solution {
         int answer = Integer.MAX_VALUE;
         
         for(int i=0; i<wires.length; i++){
-            List<List<Integer>> tops = new ArrayList<>();
-            Queue<Integer> queue = new LinkedList<>();
+            List<List<Integer>> nodes = new ArrayList<>();
+            int cnt = 1;
             boolean[] visited = new boolean[n+1];
             
             for(int j=0; j<=n; j++){
-                tops.add(new ArrayList<>());
+                nodes.add(new ArrayList<>());
             }
             
             for(int j=0; j<wires.length; j++){
-                if(i==j) continue;
+                if(i == j) continue;
                 
                 int a = wires[j][0];
                 int b = wires[j][1];
                 
-                tops.get(a).add(b);
-                tops.get(b).add(a);
+                nodes.get(a).add(b);
+                nodes.get(b).add(a);
             }
             
+            Queue<Integer> queue = new LinkedList<>();
             queue.offer(1);
             visited[1] = true;
-            int cnt = 1;
-                
+            
             while(!queue.isEmpty()){
                 int now = queue.poll();
                 
-                for(int next : tops.get(now)){
-                    if(!visited[next]){
-                        queue.offer(next);
+                for(int node : nodes.get(now)){
+                    if(!visited[node]){
+                        visited[node] = true;
+                        queue.offer(node);
                         cnt++;
-                        visited[next] = true;
                     }
                 }
             }
             
             answer = Math.min(answer, Math.abs(cnt - (n - cnt)));
-            
         }
-        
-        
-        
         return answer;
     }
 }
