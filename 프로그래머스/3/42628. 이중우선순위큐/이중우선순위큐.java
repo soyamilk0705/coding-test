6fb425e05a910d60bfe6da773e7acac4f01822e4
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-06-27 18:02:37 +0900

    [level 3] Title: 이중우선순위큐, Time: 40.81 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -12,20 +12,14 @@ class Solution {
                 int num = Integer.parseInt(oper.split(" ")[1]);
                 ascPQ.offer(num);
                 descPQ.offer(num);
-            } else if(oper.equals("D 1")){
-                if(!descPQ.isEmpty()){
-                    int n = descPQ.poll();
-                    ascPQ.remove(n);
-                }
-            } else {
-                if(!ascPQ.isEmpty()){
-                    int n = ascPQ.poll();
-                    descPQ.remove(n);
-                }
+            } else if(!descPQ.isEmpty() && oper.equals("D 1")){
+                ascPQ.remove(descPQ.poll());
+            } else if(!ascPQ.isEmpty() && oper.equals("D -1")) {
+                descPQ.remove(ascPQ.poll());
             }
         }
         
-        if(ascPQ.isEmpty() || descPQ.isEmpty()){
+        if(ascPQ.isEmpty() && descPQ.isEmpty()){
            return new int[] {0, 0}; 
         }
         
