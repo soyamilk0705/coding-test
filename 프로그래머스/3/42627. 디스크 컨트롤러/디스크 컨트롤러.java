956cabc175dc560656e62f42d09c9fa2c3cc100a
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-07-05 21:02:47 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.48 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        int answer = 0;
         
-        int time = 0;
-        int sum = 0;
-        int idx = 0;
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
         
         PriorityQueue<int[]> pq = new PriorityQueue<>(
             (a, b) -> a[1] - b[1]
         );
         
-        while(idx < jobs.length || !pq.isEmpty()){
+        int time = 0;
+        int idx = 0;
+        int cnt = 0;
+        
+        while(cnt < jobs.length){ 
             while(idx < jobs.length && jobs[idx][0] <= time){
                 pq.offer(jobs[idx]);
                 idx++;
@@ -20,13 +22,16 @@ class Solution {
             
             if(!pq.isEmpty()){
                 int[] job = pq.poll();
+                
                 time += job[1];
-                sum += (time - job[0]);
+                answer += time - job[0];
+                cnt++;
             } else {
                 time = jobs[idx][0];
             }
+            
         }
         
-        return sum / jobs.length;
+        return answer / jobs.length;
     }
 }
