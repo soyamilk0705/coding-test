1d379560e6254c648fcec7482380c90e094b33dd
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-07-22 15:45:43 +0900

    [level 2] Title: 숫자 변환하기, Time: 17.09 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int x, int y, int n) {
         int[] dp = new int[y+1];
-        
         Arrays.fill(dp, Integer.MAX_VALUE);
         
         dp[x] = 0;
@@ -14,19 +13,17 @@ class Solution {
             }
             
             if(i+n <= y){
-                dp[i+n] = Math.min(dp[i+n], dp[i] + 1);
+                dp[i+n] = Math.min(dp[i+n], dp[i]+1);
             }
             
             if(i*2 <= y){
                 dp[i*2] = Math.min(dp[i*2], dp[i]+1);
             }
             
-            if(i*3 <=y){
+            if(i*3 <= y){
                 dp[i*3] = Math.min(dp[i*3], dp[i]+1);
             }
-        
         }
-        
         
         if(dp[y] == Integer.MAX_VALUE){
             return -1;
