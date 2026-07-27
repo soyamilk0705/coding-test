8b0c04725016440bdd485fac466d011e9dcadcf2
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-07-27 14:54:20 +0900

    [level 3] Title: 입국심사, Time: 29.10 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,31 +2,34 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
+        long answer = 0;
+        
         Arrays.sort(times);
         
-        long left = 1;
-        long right = (long) n * times[times.length-1];
-        long answer = right;
+        long left = 0;
+        long right = (long) times[times.length-1] * n;
         
         while (left <= right){
             long mid = (left + right) / 2;
             
-            long people = 0;
-            for(int i=0; i<times.length; i++){
-                people += mid / times[i];
+            long cnt = 0;
+            for(int time : times){
+                cnt += mid / time;
                 
-                if(people >= n){
+                if(cnt > n){
                     break;
                 }
             }
             
-            if(people >= n){
-                answer = mid;
+            if(cnt >= n){
                 right = mid - 1;
-            } else { 
+                answer = mid;
+            } else{
                 left = mid + 1;
             }
+            
         }
+        
         
         return answer;
     }
