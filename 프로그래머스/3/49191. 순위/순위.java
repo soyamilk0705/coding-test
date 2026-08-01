691ec44a56e00e79152833c594a2d553d8768761
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-08-01 15:36:00 +0900

    [level 3] Title: 순위, Time: 4.79 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,8 +1,6 @@
-import java.util.*;
-
-
 class Solution {
     public int solution(int n, int[][] results) {
+        int answer = 0;
         boolean[][] graph = new boolean[n+1][n+1];
         
         for(int[] result : results){
@@ -19,20 +17,15 @@ class Solution {
             }
         }
         
-        int answer = 0;
-        
         for(int i=1; i<=n; i++){
-            int count = 0;
-            
+            int cnt = 0;
             for(int j=1; j<=n; j++){
-                if(i==j) continue;
-                
                 if(graph[i][j] || graph[j][i]){
-                    count++;
+                    cnt++;
                 }
             }
             
-            if(count == n - 1){
+            if(cnt == n-1){
                 answer++;
             }
         }
