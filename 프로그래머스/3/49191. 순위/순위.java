d804e8145cae328906112b95532c5c0a22698d37
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-08-04 16:17:11 +0900

    [level 3] Title: 순위, Time: 6.08 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,25 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int[][] results) {
         int answer = 0;
         boolean[][] graph = new boolean[n+1][n+1];
-        
+    
         for(int[] result : results){
             graph[result[0]][result[1]] = true;
         }
         
-        for(int k=1; k<=n; k++){
-            for(int i=1; i<=n; i++){
-                for(int j=1; j<=n; j++){
-                    if(graph[i][k] && graph[k][j]){
+        for(int x=0; x<=n; x++){
+            for(int i=0; i<=n; i++){
+                for(int j=0; j<=n; j++){
+                    if(graph[i][x] && graph[x][j]){
                         graph[i][j] = true;
                     }
                 }
             }
         }
         
-        for(int i=1; i<=n; i++){
+        for(int i=0; i<=n; i++){
             int cnt = 0;
-            for(int j=1; j<=n; j++){
+            
+            for(int j=0; j<=n; j++){
                 if(graph[i][j] || graph[j][i]){
                     cnt++;
                 }
