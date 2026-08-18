df437e4aa1da4ff0b73b5d33f2fcb025139a0fd7
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-08-18 16:52:57 +0900

    [level 2] Title: 카카오프렌즈 컬러링북, Time: 12.64 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
+++ 프로그래머스/2/1829. 카카오프렌즈 컬러링북/카카오프렌즈 컬러링북.java
@@ -3,58 +3,49 @@ import java.util.*;
 class Solution {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static boolean[][] visited;
     
     public int[] solution(int m, int n, int[][] picture) {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
 
         int[] answer = new int[2];
-        visited = new boolean[m][n];
+        
+        boolean[][] visited = new boolean[m+1][n+1];
         
         Queue<int[]> queue = new LinkedList<>();
-       
         
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                int size = 0;
-                
                 if(!visited[i][j] && picture[i][j] != 0){
-                    numberOfArea++;
-                    queue.add(new int[]{i, j});
+                    queue.offer(new int[]{i, j});
                     visited[i][j] = true;
-                    size += bfs(queue, picture, m, n);
-                }
+                    int cnt = 1;
+                    
+                    while(!queue.isEmpty()){
+                        int[] now = queue.poll();
+            
+                        for(int z=0; z<4; z++){
+                            int nx = now[0] + dx[z];
+                            int ny = now[1] + dy[z];
                 
-                maxSizeOfOneArea = Math.max(maxSizeOfOneArea, size);
+                            if(nx >= 0 && ny >=0 && nx < m && ny < n && !visited[nx][ny] && picture[now[0]][now[1]] == picture[nx][ny]){
+                                visited[nx][ny] = true;
+                                queue.offer(new int[]{nx, ny});
+                                cnt++;
+                            }
+                        }
+                    }
+                    
+                    numberOfArea++;
+                    maxSizeOfOneArea = Math.max(maxSizeOfOneArea, cnt);
+
+                }
             }
         }
         
         answer[0] = numberOfArea;
         answer[1] = maxSizeOfOneArea;
+        
         return answer;
-    }
-    
-    public int bfs(Queue<int[]> queue, int[][] picture, int m, int n){
-        int cnt = 1;
-        
-        while(!queue.isEmpty()){
-            int[] now = queue.poll();
-            int x = now[0];
-            int y = now[1];
-            
-            for(int i=0; i<4; i++){
-                int nx = dx[i] + x;
-                int ny = dy[i] + y;
-                
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny] && picture[nx][ny] == picture[x][y]){
-                    visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny});
-                    cnt++;
-                }
-            }
-        }
-        
-        return cnt;
     }
 }
