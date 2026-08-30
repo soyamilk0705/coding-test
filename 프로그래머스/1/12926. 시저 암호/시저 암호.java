11df60252b13fb5dd71f8bf6616d0133cc0b78a8
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-08-30 16:36:08 +0900

    [level 1] Title: 시저 암호, Time: 14.25 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -3,15 +3,19 @@ import java.util.*;
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
+        
+        // A = 65
+        
         for(char c : s.toCharArray()){
             if(c == ' '){
-                answer += " ";
-            } else if(Character.isUpperCase(c)){
-                answer += (char) ((c - 'A' + n) % 26 + 'A'); 
-            } else {
+                answer += ' ';
+            } else if(c >= 'A' && c <= 'Z'){
+                answer += (char) ((c - 'A' + n) % 26 + 'A');
+            } else{
                 answer += (char) ((c - 'a' + n) % 26 + 'a');
             }
         }
+        
         
         return answer;
     }
