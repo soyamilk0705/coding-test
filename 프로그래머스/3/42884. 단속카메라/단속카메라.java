4cedc2ad0f4466ab0b7a776a5cd76019ec6223a6
Heesoo Kim <70507878+soyamilk0705@users.noreply.github.com>
2026-09-08 15:14:20 +0900

    [level 3] Title: 단속카메라, Time: 4.17 ms, Memory: 59.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -3,17 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
         int answer = 0;
-        
         Arrays.sort(routes, (a, b) -> a[1] - b[1]);
-        int now = Integer.MIN_VALUE;
         
-        for(int i=0; i<routes.length; i++){
-            if(routes[i][0] > now){
-                now = routes[i][1];
+        int camera = Integer.MIN_VALUE;
+        
+        for(int[] route : routes){
+            if(camera < route[0]){
+                camera = route[1];
                 answer++;
-            } 
+            }    
         }
-        
         
         return answer;
     }
